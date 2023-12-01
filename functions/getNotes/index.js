5fd85d02f68f92a93a73582c976caffa7db909ee
Philip Miranda <philipmir@hotmail.com>
2023-12-01 10:53:00 +0100

    update, the notes are now stored in a database for the logged in user. added middy 4.7.0

--- functions/getNotes/index.js
+++ functions/getNotes/index.js
@@ -1,31 +1,38 @@
+const { sendResponse } = require('../../responses/index');
 const AWS = require('aws-sdk');
-const { sendResponse } = require('../../responses');
-const middy = require('middy'); // Make sure to install 'middy' if you haven't already
-const { validateToken } = require('../middleware/auth');
 const db = new AWS.DynamoDB.DocumentClient();
+const middy = require('@middy/core');
+const { validateToken } = require('../middleware/auth');
 
 const getNotes = async (event, context) => {
-    try {
-        if (event?.error && event?.error === '401') {
-            return sendResponse(401, { success: false, message: 'Invalid token' });
-        }
+    if (event?.error && event.error === '401') {
+      return sendResponse(401, { success: false, message: 'Invalid token' });
+    }
 
-        const { Items } = await db.scan({
-            TableName: 'notesAPI',
-            FilterExpression: "attribute_exists (#id)",
-            ExpressionAttributeNames: {
-                "#id": "id"
-            }
+    const username = event.username;
+  
+    try {
+      const result = await db.scan({
+          TableName: 'notesAPI',
+          FilterExpression: '#username = :username',
+          ExpressionAttributeNames: {
+            '#username': 'username',
+          },
+          ExpressionAttributeValues: {
+            ':username': username,
+          },
         }).promise();
 
-        return sendResponse(200, { success: true, notes: Items });
+      return sendResponse(200, { success: true, notes: result.Items });
     } catch (error) {
-        console.error(error);
-        return sendResponse(500, { success: false, message: 'Internal Server Error' });
+      console.log(error);
+      return sendResponse(400, {
+        success: false,
+        message: 'Error, couldnt fetch notes',
+      });
     }
-};
-
-const handler = middy(getNotes)
-    .use(validateToken);
-
-module.exports = { handler };
+  };
+  
+  const handler = middy(getNotes).use(validateToken);
+  
+  module.exports = { handler };
--- functions/postNotes/index.js
+++ functions/postNotes/index.js
@@ -1,26 +1,56 @@
-
-const AWS = require('aws-sdk');
-const { sendResponse } = require('../../responses');
+const { sendResponse } = require("../../responses/index");
+const AWS = require("aws-sdk");
 const db = new AWS.DynamoDB.DocumentClient();
+const { nanoid } = require("nanoid");
+const middy = require('@middy/core');
+const { validateToken } = require("../middleware/auth");
 
+const maxTitleLength = 50;
+const maxTextLength = 300;
 
+const postNotes = async (event, context) => {
+  if (event?.error && event?.error === "401")
+    return sendResponse(401, {
+      success: false,
+      message: "Invalid token. Try again.",
+    });
 
-exports.handler = async (event, context) => {
-    const note = JSON.parse(event.body);
+  const note = JSON.parse(event.body);
 
-    const timestamp = new Date().getTime();
+  if (!note.title || !note.text) {
+    return sendResponse(400, {
+      success: false,
+      message: "Title and text are required",
+    });
+  }
 
-    note.id = `${timestamp}`;
+  if (note.title.length > maxTitleLength || note.text.length > maxTextLength) {
+    return sendResponse(400, {
+      success: false,
+      message: `Title cannot be more than ${maxTitleLength} characters and text cant be more than ${maxTextLength} characters`,
+    });
+  }
 
-    try {
-        await db.put({
-            TableName: 'notesAPI',
-            Item: note
-        }).promise();
+  const date = new Date().toISOString();
+  note.id = nanoid();
+  note.createdAt = `${date}`
+  note.modifiedAt = date
+  note.username = event.username;
 
-        return sendResponse(200, {success: true});
-    } catch (error) {
-        
-        return sendResponse(500, {success: false});
-    }
-}
+  try {
+    await db
+      .put({
+        TableName: "notesAPI",
+        Item: note,
+      })
+      .promise();
+
+    return sendResponse(200, { success: true, note });
+  } catch (error) {
+    return sendResponse(500, { success: false, message: "Internal server error" });
+  }
+};
+
+const handler = middy(postNotes).use(validateToken);
+
+module.exports = { handler };
