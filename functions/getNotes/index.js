092e3632fd6c0d97303e76019ac14ee7370477d2
Philip Miranda <philipmir@hotmail.com>
2023-12-01 09:49:22 +0100

    Have to fix so you only see your own notes. added insomnia file for testing API

--- functions/getNotes/index.js
+++ functions/getNotes/index.js
@@ -1,18 +1,31 @@
 const AWS = require('aws-sdk');
 const { sendResponse } = require('../../responses');
+const middy = require('middy'); // Make sure to install 'middy' if you haven't already
+const { validateToken } = require('../middleware/auth');
 const db = new AWS.DynamoDB.DocumentClient();
 
+const getNotes = async (event, context) => {
+    try {
+        if (event?.error && event?.error === '401') {
+            return sendResponse(401, { success: false, message: 'Invalid token' });
+        }
 
+        const { Items } = await db.scan({
+            TableName: 'notesAPI',
+            FilterExpression: "attribute_exists (#id)",
+            ExpressionAttributeNames: {
+                "#id": "id"
+            }
+        }).promise();
 
-    
+        return sendResponse(200, { success: true, notes: Items });
+    } catch (error) {
+        console.error(error);
+        return sendResponse(500, { success: false, message: 'Internal Server Error' });
+    }
+};
 
-exports.handler = async (event, context) => {
+const handler = middy(getNotes)
+    .use(validateToken);
 
-    const {Items} = await db.scan({
-        TableName: 'notesAPI'
-    }).promise();
-
-    return sendResponse (200, {success : true, notes : Items});
-
-
-}
+module.exports = { handler };
